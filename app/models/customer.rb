2b6716fd6f121be7fdd560396b38716326b161ef
Francisco Quintero <frajaquico@yahoo.es>
2016-02-23 14:01:12 -0500

    add methods to get customer's data

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,13 @@ class Customer < ActiveRecord::Base
   validates :email, presence: {message: "Correo electrónico es requerido"}
 
   accepts_nested_attributes_for :body_measures, allow_destroy: true
+
+  def pretty_name
+    "#{self.first_name} #{self.last_names}"
+  end
+
+  def first_body_measure
+    self.body_measures.first
+  end
+
 end
